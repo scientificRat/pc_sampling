faf05bab2211b293ddda95ea8dcc5ee809688cf8
scientificRat <huangzhengyue.1996@gmail.com>
2018-08-03 14:17:22 +0800

    some updates

--- sampling_pc_for_modelnet40.py
+++ sampling_pc_for_modelnet40.py
@@ -2,17 +2,21 @@ import os
 import glob
 import numpy as np
 import os.path as osp
-from . import read_off, triangles_to_pc
+from load_off import read_off
+from tf_sampling import triangles_to_pc
 from tqdm import tqdm
 
-modelnet40_data_root=''
-pc_data_root=''
+modelnet40_data_root = '/repository/ModelNet40'
+pc_data_root = '/repository/pc'
+
 
 def check_dir(dir):
     if not osp.exists(dir):
         os.makedirs(dir)
 
+
 if __name__ == '__main__':
+    os.environ['CUDA_VISIBLE_DEVICES'] = '4,5'
     shape_all = glob.glob(osp.join(modelnet40_data_root, '*', '*', '*.off'))
     for shape_dir in tqdm(shape_all):
         pc_dir = shape_dir.replace(modelnet40_data_root, pc_data_root)
@@ -26,5 +30,3 @@ if __name__ == '__main__':
         triangles = read_off(shape_dir)
         ret = triangles_to_pc(triangles)
         np.save(pc_dir, ret)
-
-
